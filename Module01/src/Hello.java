3f510e53ab66019921a05dd6eba75f112d3c7a37
iuijckj <2175298805@qq.com>
2022-07-14 14:30:39 +0800

    Initial commit

---
+++ Module01/src/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * @author shkstart
+ * @date 2022/7/14 - 12:24
+ * @create 2022-07-14 12:24
+ */
+public class Hello {
+
+}
---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,17 @@
+package com.atguigu.bean;
+
+
+import java.util.ArrayList;
+
+public class Customer {
+    
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!!!");
+
+        ArrayList list = new ArrayList();
+
+
+
+    }
+}
